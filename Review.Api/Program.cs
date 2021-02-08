62cec3323a1be973290816dc275acd3f99270d47
Karim Vally <karim.vally@outlook.com>
2021-02-08 19:31:03 +0000

    Added docker-compose and modified dockerfile's of api projects.

--- Review.Api/Program.cs
+++ Review.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Review.Api
@@ -12,9 +13,6 @@ namespace Review.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
